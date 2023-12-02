5df2cc0d2785607430d242fc5f67e82756bce618
Brandon J <68445378+BJennin4@users.noreply.github.com>
2023-12-01 22:07:01 -0500

    Files updated
    Setup complete

--- main.cpp
+++ main.cpp
@@ -4,11 +4,11 @@
 Allocation:
 A B C
 _____
-0 1 0
-2 0 0
-3 0 2
-2 1 1
-0 0 2
+0 1 0 = P0
+2 0 0 = P1
+3 0 2 = P2
+2 1 1 = P3
+0 0 2 = P4
 
 Max:
 7 5 3
@@ -23,6 +23,15 @@ Available:
 Is the system in a safe state? If Yes, what is the safe sequence?
 */
 
+// Available Array // (allocation + available) when Need <= Available
+// Max Array
+// Allocation Array 
+// Need Array // (max - allocation)
+
+string allocation[5] = {"010", "200", "302", "211", "002"};
+string max[5] = {"753", "322", "902", "222", "433"};
+string available[1] = {"332"};
+
 int recA = 12, recB = 5, recC = 7;
 
 int main(){
@@ -30,7 +39,7 @@ int main(){
     
     
     
-    if (!false){
+    if (!safe){
         std::cout << "The system is not in a safe state";
     }
     
