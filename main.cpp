3a9da0de2ec3aa862434b4b8b7d2e5322d950e3c
Brandon J <68445378+BJennin4@users.noreply.github.com>
2023-12-01 21:05:32 -0500

    First Commit

---
+++ main.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+
+/*
+Allocation:
+A B C
+_____
+0 1 0
+2 0 0
+3 0 2
+2 1 1
+0 0 2
+
+Max:
+7 5 3
+3 2 2
+9 0 2
+2 2 2 
+4 3 3
+
+Available:
+3 3 2
+
+Is the system in a safe state? If Yes, what is the safe sequence?
+*/
+
+int recA = 12, recB = 5, recC = 7;
+
+int main(){
+    bool safe = false;
+    
+    
+    
+    if (!false){
+        std::cout << "The system is not in a safe state";
+    }
+    
+    return 0;
+}
